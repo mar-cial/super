cfa8958447eb591f9e439248d2dba75cd4074566
mar-cial <cesarmarcialq@gmail.com>
2022-06-17 14:21:19 -0700

    terminada la página de vegetales

--- pages/super/vegetales/index.tsx
+++ pages/super/vegetales/index.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 import { json } from 'stream/consumers';
+import Footer from '../../../components/Footer';
 import vegData from '../../../data/vegetales.json';
 
 export interface Vegetales {
@@ -13,14 +15,38 @@ export interface Vegetales {
 
 const VerdurasPage: NextPage = () => {
   return (
-    <div>
-      {vegData.map((veg) => (
-        <div>
-          <p>{veg.name}</p>
-          <Image src={require(`/public/images/vegetales/${veg.image}.jpeg`)} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="grid gap-2 p-4 md:grid-cols-2 2xl:grid-cols-3">
+        {vegData.map((veg) => (
+          <div className="relative grid justify-between gap-6 p-4 border-2 md:grid-cols-2">
+            <div className="flex flex-col justify-between gap-4">
+              <header className="flex flex-col gap-6">
+                <h3 className="text-3xl font-main">{veg.name}</h3>
+                <p className="font-serif text-gray-500 ">
+                  {`${veg.description.slice(0, veg.description.indexOf('.'))}.`}
+                </p>
+              </header>
+              <div>
+                <Link href={`/super/vegetales/${veg.image}`}>
+                  <a className="p-2 text-lg font-semibold bg-yellow-200 font-main">
+                    Saber más
+                  </a>
+                </Link>
+              </div>
+            </div>
+            <div className="relative">
+              <Image
+                src={`/images/vegetales/${veg.image}.jpeg`}
+                layout="responsive"
+                width={200}
+                height={200}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+      <Footer />
+    </>
   );
 };
 
