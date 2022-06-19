024bf8f12804863cf1be6e604aa0d311aba2cee6
mar-cial <cesarmarcialq@gmail.com>
2022-06-19 13:26:45 -0700

    footer done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,30 @@
-
-import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { AiOutlineShoppingCart, AiOutlineMail } from 'react-icons/ai';
 const Footer = () => {
   return (
-    <footer className='flex flex-col items-center justify-center bg-gray-800 h-96'>
-      <h2 className='py-6 text-3xl text-center text-white font-main'>Contáctenos</h2>
+    <footer className="flex flex-col items-center justify-center gap-4 text-white bg-gray-800 h-96">
+      <h2 className="text-3xl text-center text-white font-main">Contáctenos</h2>
 
-      <AiOutlineShoppingCart className='text-5xl text-whiteq'/>
+      <AiOutlineShoppingCart className="text-6xl" />
+
+      <div className="flex flex-col gap-1 text-center">
+        <p>Calle 123 Col. Zaragoza</p>
+        <p>Mexicali, Baja California c.p. 21300</p>
+        <p>México</p>
+      </div>
+
+      <div>
+        <button>
+          <AiOutlineMail className="text-3xl" />
+        </button>
+      </div>
+
+      <div>
+        <a href="https://github.com/mar-cial" className="font-mono">
+          Hecho por César Marcial - Github
+        </a>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
