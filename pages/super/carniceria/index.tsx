0b5f13093613e36e5656d0e03a5be90251c593db
mar-cial <cesarmarcialq@gmail.com>
2022-06-19 19:15:03 -0700

    added video banner to meats-page

--- pages/super/carniceria/index.tsx
+++ pages/super/carniceria/index.tsx
@@ -1,8 +1,6 @@
 import {
   GetServerSideProps,
-  GetStaticProps,
   InferGetServerSidePropsType,
-  InferGetStaticPropsType,
   NextPage,
 } from 'next';
 import Image from 'next/image';
@@ -33,15 +31,17 @@ const CarniceriaPage: NextPage = ({
   return (
     <div className="flex flex-col gap-6">
       <div className="relative">
-        <div className="relative z-20">
-          <video loop autoPlay muted className={'w-full'}>
-            <source src={'/videos/bbq.mp4'} type={'video/mp4'} />
-          </video>
-        </div>
-        <div className="fixed z-30 bg-black">
-          <h2>hello</h2>
+        <video loop autoPlay muted controls={false}>
+          <h3 className="text-4xl font-main">hello?</h3>
+          <source src={'/videos/bbq.mp4'} type={'video/mp4'} />
+        </video>
+
+        <div className="absolute top-0 flex flex-col items-center justify-center w-full h-full gap-4 text-center text-white bg-black bg-opacity-50 font-main">
+          <h2 className={'text-3xl md:text-5xl'}>Carnicería Súper</h2>
+          <p>Cortes gourmet, realizados por carniceros expertos.</p>
         </div>
       </div>
+
       <div className={'grid grid-cols-3 gap-6'}>
         {all.map((meat) => (
           <div className="relative w-32 h-32" key={meat.id}>
