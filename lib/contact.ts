a4524d8dbeb8050fc18db06620f4814526d690df
mar-cial <cesarmarcialq@gmail.com>
2022-06-23 14:51:23 -0700

    created lib for general purpose functions

---
+++ lib/contact.ts
@@ -0,0 +1,10 @@
+const contact = () =>
+  alert(
+    `Gracias por checar este proyecto! Por ahora es un prototipo, por lo que no tiene funcionalidad todavía.\nVisite mi github.com para contactarme o para conocer otros proyectos mios:\n
+    
+  https://github.com/mar-cial
+    
+  `
+  );
+
+export default contact;
