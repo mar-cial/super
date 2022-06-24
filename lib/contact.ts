396ea05355857f3fdb5223231395147e582a6489
mar-cial <cesarmarcialq@gmail.com>
2022-06-24 09:42:30 -0700

    removed some whitespace

--- lib/contact.ts
+++ lib/contact.ts
@@ -1,3 +1,9 @@
+/**
+ * Displays an alert showing my contact information.
+ *
+ * Serves as a placeholder for actual web app funtionality.
+ * @returns {void} -
+ */
 const contact = () =>
   alert(
     `Gracias por checar este proyecto! Por ahora es un prototipo, por lo que no tiene funcionalidad todavía.\nVisite mi github.com para contactarme o para conocer otros proyectos mios:\n
